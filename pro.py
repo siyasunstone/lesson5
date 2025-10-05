efac20e8b06292abb3e8b7f2993a22c957364bd4
siyasunstone <patilsiya491@gmail.com>
2025-10-05 10:04:21 +0100

    hi their

--- pro.py
+++ pro.py
@@ -4,7 +4,7 @@ number = random.randint(1, 100)
 attempts = 0
 guess = 0
 
-while guess != number and attempts < 12:
+while guess != number and attempts < 3:
     guess = int(input("Enter a number: "))
     attempts += 1
 
@@ -14,7 +14,7 @@ while guess != number and attempts < 12:
         print("Too high!")
     else:
         print(f"🎉 Correct! You guessed it in {attempts} tries.")
-
+4
 # after the loop ends, check why
 if guess != number:
     print("😢 You used all 12 tries. You lose! The number was", number)
---
+++ profic.py
@@ -0,0 +1,3 @@
+a,b,c=map(int,input("Enter three numbers: ").split())
+a,b,c=c,a,b
+print(a,b,c)

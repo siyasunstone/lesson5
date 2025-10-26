ca93795cbe2856b9c921393f7cb1bcb7910f5b7c
siyasunstone <patilsiya491@gmail.com>
2025-10-26 10:57:43 +0000

    hi

--- hi.py
+++ hi.py
@@ -1,5 +1,1 @@
 
-rows = int(input("Enter the number of rows: "))
-
-for i in range(1, rows + 1):
-    print(" " * (rows - i) + "*" * i)
--- homework.py
+++ homework.py
@@ -1,7 +1,15 @@
-import math
+import turtle
+turtle.Screen().bgcolor("light pink")
+turtle.Screen().setup(400,600)
+pentagon=turtle.Turtle()
+sides=5
+length=50
+angle=360/sides
+for i in range(sides):
+    pentagon.forward(length)
+    pentagon.left(angle)
+turtle.done()
 
-n = float(input("Enter a number: "))
-if n >= 0:
-    print("Square root is", math.sqrt(n))
-else:
-    print("Cannot find square root of a negative number.")
+
+
+

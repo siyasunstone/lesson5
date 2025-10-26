5de7c3f26bb38d17a823c814b8fb449a27a6c612
siyasunstone <patilsiya491@gmail.com>
2025-10-26 11:02:55 +0000

    ho

--- homework.py
+++ homework.py
@@ -1,13 +1,13 @@
 import turtle
-turtle.Screen().bgcolor("light pink")
+turtle.Screen().bgcolor("sky blue")
 turtle.Screen().setup(400,600)
-pentagon=turtle.Turtle()
-sides=5
+squre=turtle.Turtle()
+sides=4
 length=50
 angle=360/sides
 for i in range(sides):
-    pentagon.forward(length)
-    pentagon.left(angle)
+    squre.forward(length)
+    squre.left(angle)
 turtle.done()
 
 
